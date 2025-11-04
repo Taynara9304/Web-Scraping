e10f768611ccea1e25cc9c174b1c82bbe60334da
Taynara <taynaradasilva2001@gmail.com>
2025-11-04 13:39:40 +0000

    correção

--- webScraping/scraper.py
+++ webScraping/scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 url = "https://www.booking.com/searchresults.pt-br.html?label=gen173nr-10CAEoggI46AdIM1gEaCCIAQGYATO4ARnIAQzYAQPoAQH4AQGIAgGoAgG4AvTpp8gGwAIB0gIkNTc2OTU0ODYtMjUxZi00OTg4LTliZDItNWUxNTBiYjk3ODMw2AIB4AIB&aid=304142&checkin=2025-11-28&checkout=2025-12-01&dest_id=-639568&dest_type=city&order=popularity&group_adults=5&req_adults=5&no_rooms=1&group_children=0&req_children=0"
 
-response = requests.get(URL, timeout=10)
+response = requests.get(url, timeout=10)
 response.raise_for_status()
 
 print("Requisição bem-sucedida! Status Code:", response.status_code)
